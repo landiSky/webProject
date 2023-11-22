3e62cbdea6a9ecb492fb3450ae76d8cabee3ac6d
landiSky <815145688@qq.com>
2023-11-22 17:17:36 +0800

    feat: 卖家-订单管理-详情部分更改

--- src/views/home/seller/order/components/details-modal-fullscreen.vue
+++ src/views/home/seller/order/components/details-modal-fullscreen.vue
@@ -63,7 +63,7 @@
             </div>
             <div class="statusinfo">
               <div v-if="dataList.orderStatus === 0">
-                <t-space class="order-success">
+                <t-space class="order-success spacing">
                   <div class="order-success-icon is-primary">
                     <icon-clock-circle-fill />
                   </div>
@@ -74,7 +74,7 @@
                 </t-space>
               </div>
               <div v-if="dataList.orderStatus === 1">
-                <t-space class="order-success">
+                <t-space class="order-success spacing">
                   <div class="order-success-icon is-warning">
                     <icon-clock-circle-fill />
                   </div>
@@ -82,22 +82,22 @@
                   <div class="order-success-text"
                     >待审核：买家已提交支付凭证，请进行审核。
                   </div>
-                  <div class="return-ok">
-                    <t-button
-                      type="primary"
-                      status="danger"
-                      style="margin-right: 10px"
-                      @click="turndownsyhn"
-                      >驳回</t-button
-                    >
-                    <t-button type="primary" @click="passok(dataList.id)"
-                      >通过</t-button
-                    >
-                  </div>
                 </t-space>
+                <div class="return-ok">
+                  <t-button
+                    type="primary"
+                    status="danger"
+                    style="margin-right: 10px"
+                    @click="turndownsyhn"
+                    >驳回</t-button
+                  >
+                  <t-button type="primary" @click="passok(dataList.id)"
+                    >通过</t-button
+                  >
+                </div>
               </div>
               <div v-if="dataList.orderStatus === 4">
-                <t-space class="order-success">
+                <t-space class="order-success spacing">
                   <div class="order-success-icon is-danger">
                     <icon-close-circle-fill />
                   </div>
@@ -133,7 +133,7 @@
                 </div>
               </div>
               <div v-if="dataList.orderStatus === 2">
-                <t-space class="order-success">
+                <t-space class="order-success spacing">
                   <div class="order-success-icon is-warning">
                     <icon-clock-circle-fill />
                   </div>
@@ -141,11 +141,13 @@
                   <div class="order-success-text">
                     待交付：已确认收款，请完成订单交付。
                   </div>
-                  <t-button type="primary" @click="delivery">立即交付</t-button>
                 </t-space>
+                <div>
+                  <t-button type="primary" @click="delivery">立即交付</t-button>
+                </div>
               </div>
               <div v-if="dataList.orderStatus === 5">
-                <t-space class="order-success">
+                <t-space class="order-success spacing">
                   <div class="order-success-icon is-primary">
                     <icon-clock-circle-fill />
                   </div>
@@ -156,7 +158,7 @@
                 </t-space>
               </div>
               <div v-if="dataList.orderStatus === 3">
-                <t-space class="order-success">
+                <t-space class="order-success spacing">
                   <div class="order-success-icon">
                     <icon-check-circle-fill />
                   </div>
@@ -827,29 +829,55 @@ onMounted(() => {
   }
 }
 
-.order-item-deploy {
-  margin-top: 16px;
+:deep(.tele-space-item) {
+  margin-right: 12px !important;
 }
 
-.order-item-due-date {
-  margin-top: 12px;
+.spacing {
+  :deep(.tele-space-item) {
+    margin-right: 8px !important;
+  }
+}
+
+.order-success {
+  margin-bottom: 16px;
+  color: var(--t-41-d-2129, #1d2129);
+  font-weight: 500;
+  font-size: 14px;
+
+  /* CN/标题/14-Medium-迷你 */
+  font-family: PingFang SC;
+  font-style: normal;
+  line-height: 22px; /* 157.143% */
+}
+
+.order-item-due-date,
+.order-item-deploy {
+  margin-bottom: 12px;
 }
 
 .order-item-left {
-  margin-right: 12px;
-  color: #4e5969;
+  color: var(--f-64-e-5969, #4e5969);
   font-weight: 400;
   font-size: 12px;
+  font-family: PingFang SC;
   font-style: normal;
-  line-height: 20px;
+  line-height: 20px; /* 166.667% */
   text-align: right;
 }
 
 .order-item-right {
-  color: #1d2129;
+  color: var(--f-71-d-2129, #1d2129);
   font-weight: 400;
   font-size: 12px;
+
+  /* 12/CN-Regular-pingfang */
+  font-family: PingFang SC;
   font-style: normal;
-  line-height: 20px;
+  line-height: 20px; /* 166.667% */
+}
+
+.button-top {
+  margin-top: 4px;
 }
 </style>
