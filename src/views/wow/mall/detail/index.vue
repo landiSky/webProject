6320b1630adb6799603730fd8a27a829d52e0bdf
landiSky <815145688@qq.com>
2023-11-23 16:37:51 +0800

    chore: 修改文案

--- src/views/wow/mall/detail/index.vue
+++ src/views/wow/mall/detail/index.vue
@@ -355,7 +355,7 @@ const clickAddCart = (): void => {
   if (userInfoByCompany?.primary === AccountType.UNAUTH) {
     Modal.info({
       title: '使用提醒',
-      content: '需申请企业认证后使用，请先进行企业节点认证。',
+      content: '需申请企业认证后使用，请先进行企业认证。',
       titleAlign: 'start',
       hideCancel: false,
       cancelText: '暂不认证',
