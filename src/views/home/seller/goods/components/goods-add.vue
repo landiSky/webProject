1e08a3e97d0f7ec54e19d71f9779897b3a75a6e0
landiSky <815145688@qq.com>
2024-06-15 09:36:57 +0800

    feat: 商品管理-新增编辑商品图片大小限制更改

--- src/views/home/seller/goods/components/goods-add.vue
+++ src/views/home/seller/goods/components/goods-add.vue
@@ -182,7 +182,7 @@
           </t-form-item>
           <t-form-item label="" field="" class="hint-item">
             <div class="hint"
-              >支持jpg、jpeg、png、bmp、gif文件格式，文件大小限制2M以内。</div
+              >支持jpg、jpeg、png、bmp、gif文件格式，文件大小限制10M以内。</div
             >
           </t-form-item>
           <t-form-item
@@ -271,7 +271,7 @@
           </t-form-item>
           <t-form-item label="" field="" class="hint-item">
             <div class="hint"
-              >支持jpg、jpeg、png、bmp、gif文件格式，文件大小限制2M以内。</div
+              >支持jpg、jpeg、png、bmp、gif文件格式，文件大小限制10M以内。</div
             >
           </t-form-item>
           <t-form-item label="商品分类" field="productTypeId">
@@ -1227,8 +1227,8 @@ const uploadError = (fileItem: FileItem) => {
   logoUploading.value = false;
   formModel.value.logo = '';
   const size = fileItem.file?.size ?? 0;
-  if (size > 2 * 1024 * 1024) {
-    Message.error(`上传失败，文件大小不要超过2M`);
+  if (size > 10 * 1024 * 1024) {
+    Message.error(`上传失败，文件大小不要超过10M`);
   } else {
     Message.error(`上传失败，请检查网络`);
   }
@@ -1260,8 +1260,8 @@ const uploadDetailProgress = () => {
 const uploadDetailError = (fileItem: FileItem) => {
   detailUploading.value = false;
   const size = fileItem.file?.size ?? 0;
-  if (size > 2 * 1024 * 1024) {
-    Message.error(`上传失败，文件大小不要超过2M`);
+  if (size > 10 * 1024 * 1024) {
+    Message.error(`上传失败，文件大小不要超过10M`);
   } else {
     Message.error(`上传失败，请检查网络`);
   }
@@ -1656,9 +1656,9 @@ const beforeUpload = (file: File) => {
       Message.warning(`上传失败，请检查文件类型`);
       reject();
     }
-    const over2 = file.size > 1024 * 1024 * 2;
+    const over2 = file.size > 1024 * 1024 * 10;
     if (over2) {
-      Message.warning(`上传失败，文件大小不要超过2M`);
+      Message.warning(`上传失败，文件大小不要超过10M`);
       reject();
     }
     if (!online.value) {
