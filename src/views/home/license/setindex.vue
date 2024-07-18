fec390ef0f11fa371e3e1cc834f2646b81aeefff
landiSky <815145688@qq.com>
2024-07-18 11:03:50 +0800

    fix: license下载,下载文件名修改,通过后端进行

--- src/views/home/license/setindex.vue
+++ src/views/home/license/setindex.vue
@@ -452,10 +452,8 @@ const clickDetails = (data: any, typeindex: string) => {
 // 下载License文件
 const downloadLicense = (data: any) => {
   const link = document.createElement('a');
-  const objectUrl = `/server/web/file/licenseDownload?name=${data?.licenseUrl}`; // 创建一个新的url对象
+  const objectUrl = `/server/web/file/licenseDownload?name=${data?.licenseUrl}&code=${data.code}`; // 创建一个新的url对象
   link.href = objectUrl;
-  const fileName = `${data.code}License.json`;
-  link.download = fileName; //  下载的时候自定义的文件名
   link.click();
   window.URL.revokeObjectURL(objectUrl); // 为了更好地性能和内存使用状况，应该在适当的时候释放url
 };
