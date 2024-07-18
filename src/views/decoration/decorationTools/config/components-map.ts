32be5ebeab4f490783f14b7c3f9f972a0921489e
landiSky <815145688@qq.com>
2024-07-18 16:21:04 +0800

    feat: 更新

--- src/views/decoration/decorationTools/config/components-map.ts
+++ src/views/decoration/decorationTools/config/components-map.ts
@@ -1,13 +1,29 @@
 import { defineAsyncComponent } from 'vue';
+import View from '../components/herizontal-image-text/view.vue';
 
 const ComponentsMap: Record<string, any> = {
   SigleImg: {
     view: () =>
       defineAsyncComponent(
-        () =>
-          import(
-            '@/views/decoration/decorationTools/components/single-image/view.vue'
-          )
+        {
+          // 使用loader函数来创建异步组件
+          loader: () =>
+            import(
+              '@/views/decoration/decorationTools/components/single-image/view.vue'
+            ),
+          // 提供一个在组件加载时显示的组件
+          loadingComponent: View, // LoadingComponent是一个已定义的组件
+          // 提供一个在组件加载失败时显示的组件
+          errorComponent: View, // ErrorComponent是一个已定义的组件
+          // 设置delay为null取消自动显示的加载组件的延迟
+          delay: null,
+          // 如果提供了timeout，则这个时间过后，如果组件还没加载出来，会使用errorComponent
+          timeout: 3000,
+        }
+        // () =>
+        //   import(
+        //     '@/views/decoration/decorationTools/components/single-image/view.vue'
+        //   )
       ),
     uiConfig: () =>
       defineAsyncComponent(
@@ -20,10 +36,25 @@ const ComponentsMap: Record<string, any> = {
   VerticalImageText: {
     view: () =>
       defineAsyncComponent(
-        () =>
-          import(
-            '@/views/decoration/decorationTools/components/vertival-image-text/view.vue'
-          )
+        {
+          // 使用loader函数来创建异步组件
+          loader: () =>
+            import(
+              '@/views/decoration/decorationTools/components/vertival-image-text/view.vue'
+            ),
+          // 提供一个在组件加载时显示的组件
+          loadingComponent: View, // LoadingComponent是一个已定义的组件
+          // 提供一个在组件加载失败时显示的组件
+          errorComponent: View, // ErrorComponent是一个已定义的组件
+          // 设置delay为null取消自动显示的加载组件的延迟
+          delay: null,
+          // 如果提供了timeout，则这个时间过后，如果组件还没加载出来，会使用errorComponent
+          timeout: 3000,
+        }
+        // () =>
+        //   import(
+        //     '@/views/decoration/decorationTools/components/vertival-image-text/view.vue'
+        //   )
       ),
     uiConfig: () =>
       defineAsyncComponent(
