35675ae0a58e516abc005fc2218ba5d6efdd7db2
landiSky <815145688@qq.com>
2024-09-23 14:21:06 +0800

    feat: 公共utils中增加获取当前日期（年月日）

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -32,3 +32,15 @@ export function addId(data: Record<string, any>) {
   }
   return data;
 }
+
+// 获取当前日期函数
+export function getNowFormatDate() {
+  const date: any = new Date();
+  const year: any = date.getFullYear(); // 获取完整的年份(4位)
+  let month: any = date.getMonth() + 1; // 获取当前月份(0-11,0代表1月)
+  let strDate: any = date.getDate(); // 获取当前日(1-31)
+  if (month < 10) month = `0${month}`; // 如果月份是个位数，在前面补0
+  if (strDate < 10) strDate = `0${strDate}`; // 如果日是个位数，在前面补0
+
+  return `${year}-${month}-${strDate}`;
+}
