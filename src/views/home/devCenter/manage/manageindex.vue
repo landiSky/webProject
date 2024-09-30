f1be7cd0f0cfdd94160fc9726621929b611b67b2
landiSky <815145688@qq.com>
2024-09-30 18:33:14 +0800

    fix: 应用管理调整成跳转应用首页地址

--- src/views/home/devCenter/manage/manageindex.vue
+++ src/views/home/devCenter/manage/manageindex.vue
@@ -566,63 +566,67 @@ const handleTableDebugging = (record: Record<string, any>) => {
     window.open(record?.link);
     return false;
   }
-  const { id, productId, deliveryId, deliveryType } = record;
+  // const { id, productId, deliveryId, deliveryType } = record;
 
   if (Number(record.appType) === 1) {
-    const params = {
-      productId,
-      productDeliverySetId: deliveryId,
-      memberId: selectCompany.value?.memberId,
-      orderId: id,
-    };
-
-    alreadyBuyClientLogin(params).then((res: any) => {
-      const data = {
-        type: 'productApp',
-        productId,
-        productDeliverySetId: deliveryId,
-        memberId: selectCompany.value?.memberId,
-      };
-      const sm2data = sm2(
-        JSON.stringify(data),
-        userStore.configInfo?.publicKey
-      );
-      if (res.code === 102008) {
-        return Message.warning(res?.message);
-      }
-      if (res.code !== 200) {
-        return Message.error(res?.message);
-      }
-      if (deliveryType === 1) {
-        window.open(res.data);
-      } else {
-        window.open(`${res.data}&data=${sm2data}`);
-      }
-      return true;
-    });
-  } else if (Number(record.appType) === 0 && record.dockingMethod === 0) {
-    const params = {
-      appInfoId: id,
-      companyId: userInfoByCompany.value.companyId,
-    };
-    appInfoClientLogin(params).then((res: any) => {
-      if (res.code === 102008) {
-        return Message.warning(res?.message);
-      }
-      if (res.code !== 200) {
-        return Message.error(res?.message);
-      }
-      const data = {
-        type: 'selfApp',
-        companyId: userInfoByCompany.value.companyId,
-      };
-      const sm2data = sm2(
-        JSON.stringify(data),
-        userStore.configInfo?.publicKey
-      );
-      window.open(`${res.data}&data=${sm2data}`);
-      return true;
-    });
+    // const params = {
+    //   productId,
+    //   productDeliverySetId: deliveryId,
+    //   memberId: selectCompany.value?.memberId,
+    //   orderId: id,
+    // };
+    // alreadyBuyClientLogin(params).then((res: any) => {
+    //   const data = {
+    //     type: 'productApp',
+    //     productId,
+    //     productDeliverySetId: deliveryId,
+    //     memberId: selectCompany.value?.memberId,
+    //   };
+    //   const sm2data = sm2(
+    //     JSON.stringify(data),
+    //     userStore.configInfo?.publicKey
+    //   );
+    //   if (res.code === 102008) {
+    //     return Message.warning(res?.message);
+    //   }
+    //   if (res.code !== 200) {
+    //     return Message.error(res?.message);
+    //   }
+    //   if (deliveryType === 1) {
+    //     window.open(res.data);
+    //   } else {
+    //     window.open(`${res.data}&data=${sm2data}`);
+    //   }
+    //   return true;
+    // });
+    window.open(record?.homeUri);
+    return false;
+  }
+  if (Number(record.appType) === 0 && record.dockingMethod === 0) {
+    window.open(record?.homeUri);
+    return false;
+    // const params = {
+    //   appInfoId: id,
+    //   companyId: userInfoByCompany.value.companyId,
+    // };
+    // appInfoClientLogin(params).then((res: any) => {
+    //   if (res.code === 102008) {
+    //     return Message.warning(res?.message);
+    //   }
+    //   if (res.code !== 200) {
+    //     return Message.error(res?.message);
+    //   }
+    //   const data = {
+    //     type: 'selfApp',
+    //     companyId: userInfoByCompany.value.companyId,
+    //   };
+    //   const sm2data = sm2(
+    //     JSON.stringify(data),
+    //     userStore.configInfo?.publicKey
+    //   );
+    //   window.open(`${res.data}&data=${sm2data}`);
+    //   return true;
+    // });
   }
   return true;
 };
