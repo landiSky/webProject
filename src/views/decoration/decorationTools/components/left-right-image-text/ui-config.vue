86c67033963a6bacc7e9f000eaa771e94610bcb1
landiSky <815145688@qq.com>
2024-10-11 11:19:06 +0800

    fix: 修改左右图文组件，取消简介部分文字框的自适应高度属性

--- src/views/decoration/decorationTools/components/left-right-image-text/ui-config.vue
+++ src/views/decoration/decorationTools/components/left-right-image-text/ui-config.vue
@@ -110,9 +110,6 @@
             }"
             show-word-limit
             :allow-clear="false"
-            :auto-size="{
-              minRows: 6,
-            }"
           />
         </t-form-item>
         <t-form-item
