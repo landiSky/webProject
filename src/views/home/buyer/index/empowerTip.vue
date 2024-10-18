a96937dedb4c048bacf0ae198edea9c8be8a82fb
landiSky <815145688@qq.com>
2024-10-18 16:17:26 +0800

    feat: 买家中心-概览 授权弹窗 协议文案增加

--- src/views/home/buyer/index/empowerTip.vue
+++ src/views/home/buyer/index/empowerTip.vue
@@ -42,7 +42,149 @@
         <!-- <icon-up /> -->
       </div>
     </div>
-    <div v-else class="text-field"> 占位 </div>
+    <div v-else class="text-field">
+      <p style="margin-left: 0; text-align: justify"
+        ><span
+          style="
+            color: black;
+            font-size: 12pt;
+            font-family: 宋体, 宋体, 宋体, 宋体;
+            background-color: initial;
+          "
+          >《用户授权协议》（以下简称“本协议”）是北京泰尔英福科技有限公司（以下简称“智数通”）与用户（以下简称“您”）所订立的有效合约。</span
+        ></p
+      >
+      <p style="margin-left: 0; text-align: justify"
+        ><span
+          style="
+            color: black;
+            font-size: 12pt;
+            font-family: 宋体, 宋体, 宋体, 宋体;
+            background-color: initial;
+          "
+          ><strong
+            >【审慎阅读】<u
+              >您即将通过本页面使用智数通平台账户授权登录第三方站点/软件或进行一定数据共享。确认授权前请您仔细阅读本协议的全部内容（特别是以粗体下划线标注的内容）。如您不同意本协议内容，或无法准确理解本协议任何条款的含义，请不要进行授权及后续操作。</u
+            ></strong
+          ></span
+        ></p
+      >
+      <p style="margin-left: 0; text-align: justify"
+        ><span
+          style="
+            color: black;
+            font-size: 12pt;
+            font-family: 宋体, 宋体, 宋体, 宋体;
+            background-color: initial;
+          "
+          ><strong
+            >【签约动作】<u
+              >您通过网络页面确认同意接受本协议，视为您同意接受本协议的全部约定内容。</u
+            ></strong
+          ></span
+        ></p
+      >
+      <p style="margin-left: 0"
+        ><span
+          style="
+            color: black;
+            font-size: 12pt;
+            font-family: 宋体, 宋体, 宋体, 宋体;
+            background-color: initial;
+          "
+          ><strong
+            >【共享信息】<u
+              >您授权智数通将您智数通平台账户的相关信息和数据传递给第三方包含但不限于（读取你的登录状态信息</u
+            ></strong
+          ></span
+        >、<span
+          style="
+            color: black;
+            font-size: 12pt;
+            font-family: 宋体, 宋体, 宋体, 宋体;
+            background-color: initial;
+          "
+          ><strong
+            ><u
+              >获取你的公开信息（登录名、昵称、头像等）使用身份认证信息（姓名/企业名称、手机号、证件号码等）使用企业认证信息（企业名称、统一社会信用代码、企业地址、法人姓名、营业执照、企业联系人姓名、联系人身份证号、联系人身份证）办理业务（如完成服务开通认证等）），用于第三方为您提供服务。页面提示上会展示具体授权对象、授权字段及内容等。</u
+            ></strong
+          >智数通会要求该第三方依法使用您的上述信息。<strong
+            ><u>如您不希望向第三方提供上述信息，请勿点击确认授权。</u></strong
+          ></span
+        ></p
+      >
+      <p style="margin-left: 0"
+        ><span
+          style="
+            color: black;
+            font-size: 12pt;
+            font-family: 宋体, 宋体, 宋体, 宋体;
+            background-color: initial;
+          "
+          ><strong
+            >【责任承担】<u
+              >您理解，第三方服务由相应第三方提供并就以上服务及数据安全独立承担责任。因该第三方服务或其使用您的信息产生的纠纷，或第三方服务违反相关法律法规，或您在使用第三方服务过程中遭受的损失，请您与第三方协商解决。</u
+            ></strong
+          ></span
+        ></p
+      >
+      <p style="margin-left: 0"
+        ><span
+          style="
+            color: black;
+            font-size: 12pt;
+            font-family: 宋体, 宋体, 宋体, 宋体;
+            background-color: initial;
+          "
+          ><strong
+            >【安全提示】<u
+              >网络欺诈往往会模仿、仿冒本登录页面的样式制作视觉感相似的页面引诱用户输入账号和密码等信息，智数通在此提醒您防范网络风险，不要向非智数通的站点或任何人士透露您的智数通平台账户的名称、密码等相关信息。</u
+            ></strong
+          ></span
+        ></p
+      >
+      <p style="margin-left: 0"
+        ><span
+          style="
+            color: black;
+            font-size: 12pt;
+            font-family: 宋体, 宋体, 宋体, 宋体;
+            background-color: initial;
+          "
+          ><strong
+            >【协议变更】<u
+              >您同意，智数通可不时修订本协议，并将协议变更情况在智数通平台的网站/客户端上进行公示，该等变更自公告载明的生效时间开始生效，并成为本协议的一部分。</u
+            ></strong
+          ></span
+        ></p
+      >
+      <p style="margin-left: 0"
+        ><span
+          style="
+            color: black;
+            font-size: 12pt;
+            font-family: 宋体, 宋体, 宋体, 宋体;
+            background-color: initial;
+          "
+          ><strong
+            >【争议解决】<u
+              >凡因本协议引起的任何与之有关的争议，首先应由该等争议的相关当事方通过友好协商解决，如果无法友好协商解决的，任何一方均可向被告所在地有管辖权的人民法院提起诉讼。</u
+            ></strong
+          ></span
+        ></p
+      >
+      <p style="margin-left: 0"
+        ><span
+          style="
+            color: black;
+            font-size: 12pt;
+            font-family: 宋体, 宋体, 宋体, 宋体;
+            background-color: initial;
+          "
+          >【其他】本协议未尽事宜，参照《用户服务协议》与本协议有冲突的，以本协议为准。</span
+        ></p
+      >
+    </div>
   </t-modal>
 </template>
 
