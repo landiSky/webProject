40a982642486f45a0c69a5f8d39e5bddcdc46874
landiSky <815145688@qq.com>
2024-10-18 11:56:58 +0800

    fix: 装修组件-图标文字 调整

--- src/views/decoration/decorationTools/components/multi-icon-text/view.vue
+++ src/views/decoration/decorationTools/components/multi-icon-text/view.vue
@@ -125,11 +125,11 @@ defineExpose({
     display: flex;
     flex-wrap: wrap;
     align-items: center;
-    justify-content: center;
+    justify-content: flex-start;
     width: calc(@factor * 720px);
     padding-bottom: calc(@factor * 20px);
     // height: calc(@factor * 260px);
-    padding-left: calc(@factor * 15px);
+    padding-left: calc(@factor * 52px);
     // background-color: rgb(168, 158, 158);
     overflow: hidden;
 
